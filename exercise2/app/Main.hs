8e30a4bbdf02a3f44d1fcc1a649bc890c77e7816
Bartek Kalinka <bartkali@hotmail.com>
2017-12-14 22:33:01 +0100

    exercise 2 quicksort 1st version

---
+++ exercise2/app/Main.hs
@@ -0,0 +1,7 @@
+module Main where
+
+import Quicksort
+
+main :: IO ()
+--main = print $ sort [4, 1, 3, 5, 2, 8]
+main = print $ sort [3, 32, 2, 1, 2, 5, 5, 321, 1, 123, 12, 3, 43, 345]
---
+++ exercise2/src/Quicksort.hs
@@ -0,0 +1,17 @@
+module Quicksort
+    ( sort
+    ) where
+
+sort :: [Int] -> [Int]
+sort [] = []
+sort (h:[]) = [h]
+sort (h:t) = 
+	let (a, b) = partition h [] [] t 
+	in (sort a) ++ (sort b)
+
+partition :: Int -> [Int] -> [Int] -> [Int] -> ([Int], [Int])
+partition pivot acc1 acc2 [] = (acc1 ++ [pivot], acc2)
+partition pivot acc1 acc2 (x: xs) = 
+	if pivot <= x
+		then partition pivot acc1 (x:acc2) xs
+		else partition pivot (x:acc1) acc2 xs
